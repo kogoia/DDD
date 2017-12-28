435c5b638d428a59736c6386520044a13edc37c2
Giorgi Koguashvili <kogo27@gmail.com>
2017-12-28 18:16:41 +0400

    refactored

---
+++ DomainDrivenDesign/DDD/Domain/Tab/Messages/Events/TabClosedEvent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using DDD.Infrastructure;
+
+namespace DDD.Domain.Tab.Events
+{
+    public class TabClosedEvent : IEvent<Tab>
+    {
+        public decimal Price { get; }
+        public TabClosedEvent(decimal price)
+        {
+            Price = price;
+        }
+
+		public Tab Handle(Tab model)
+		{
+            return model
+                .Match(
+                    (dTab) => throw new Exception("Can't close DefaultTab"),
+                    (oTab) => new Tab(oTab.Closed(Price)),
+                    (cTab) => throw new Exception("Can't close ClosedTab")
+                );
+        }
+
+        public string Serialazed()
+        {
+	        throw new System.NotImplementedException();
+        }
+	}
+}
